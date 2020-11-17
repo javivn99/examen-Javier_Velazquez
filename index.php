d6dd4294b309367655f0e5f7ddfd4ffa683214bf
javivn99 <javiyosoyyo@gmail.com>
2020-11-17 17:41:50 +0100

    añado url de heroku

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ echo'
                     <h5>Enlace Heroku </h5>
                     Pulsa sobre esta imagen para ver desplegada la aplicacion sobre heroku
                     <p>
-                    <a title="Heroku" href=""><img src="imagenes/heroku.png" alt="Heroku" width="120" height="120" /></a>
+                    <a title="Heroku" href="https://examen-javiervelazquez.herokuapp.com/"><img src="imagenes/heroku.png" alt="Heroku" width="120" height="120" /></a>
                     </p>
         </aside>
         <form class="col s12" method = "POST">
